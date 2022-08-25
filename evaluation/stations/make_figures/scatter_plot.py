dfcd0e4b599c8b6452865fb7e7901ac1b5ed1f79
AdrienDams <adriendamseaux@hotmail.fr>
2022-08-25 14:10:52 +0200

    depths for each month

--- evaluation/stations/make_figures/scatter_plot.py
+++ evaluation/stations/make_figures/scatter_plot.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from datetime import date
 import os
+from os.path import exists
 import matplotlib.pyplot as plt
 import sys
 import seaborn as sns
@@ -11,12 +12,12 @@ from scipy import stats
 from extract_functions import *
 
 # ----------------------------- Open data ---------------------------------
-output_dir = os.environ['cegio'] + "/figures/" + os.environ['run_name'] + "scatter/"
+output_dir = os.environ['cegio'] + "/figures/" + os.environ['run_name'] + "/scatter/"
 os.makedirs(output_dir, exist_ok=True)
 
 # Load file
-#stationfile = sys.argv[1]
-stationfile = "/work/aa0049/a271098/cegio/data/stations/57_DOM02_004/stations-vs-ctsm.1979-2019.tmp.57_DOM02_004.nc"
+stationfile = sys.argv[1]
+#stationfile = "/work/aa0049/a271098/cegio/data/stations/57_DOM02_004/stations-vs-ctsm.1979-2019.tmp.57_DOM02_004.nc"
 dataset = nc.Dataset(stationfile)
 
 # Load variables
@@ -27,6 +28,7 @@ data_sim   = dataset.variables['ctsm_soiltemp'][:].T
 data_depth = dataset.variables['depth'][:]
 data_lon   = dataset.variables['lon'][:]
 data_lat   = dataset.variables['lat'][:]
+nmonths    = 12
 
 dataset.close()
 
@@ -38,8 +40,9 @@ date_list = Date_transformer(data_time,1979)
 # We check whether we have data from every month of a year for every grid point,
 # for all years. If this is not the case, this specific the data from that year
 # gets discarded. This creates a results.csv file.
-#master = Extract_Depths(data_obs,data_sim,date_list,data_depth,data_lon,data_lat)
-
+if exists("results.csv") == False:
+	master = Extract_Depths(data_obs,data_sim,date_list,data_depth,data_lon,data_lat)
+	print("Data extracted!")
 # --------------------------------------------------------------------------
 
 # -------------------------- DATA-Import from CSV --------------------------
@@ -66,19 +69,19 @@ simulation  = np.round(all_result['simulation'],2)
 color_months = ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c',
                 '#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928']
 label_months = ['January','February','March','April','May','June',
-            	'Juli','August','September','October','November','December']
+            	'July','August','September','October','November','December']
 
 # ----------------------------- Initialize plot ---------------------------- 
 fig, ax = plt.subplots()
 fig.set_figheight(10)
 fig.set_figwidth(15)
-r_value = np.zeros(12)
+r_value = np.zeros(nmonths)
 
-for i in range(12):
-	slope, intercept, r_value[i], p_value, std_err = stats.linregress(measurement[i:-1:12],simulation[i:-1:12])
+for i in range(nmonths):
+	slope, intercept, r_value[i], p_value, std_err = stats.linregress(measurement[i:-1:nmonths],simulation[i:-1:nmonths])
 
-	sns.regplot(x = measurement[i:-1:12],
-				y = simulation[i:-1:12],
+	sns.regplot(x = measurement[i:-1:nmonths],
+				y = simulation[i:-1:nmonths],
 				color = color_months[i],
 				label = label_months[i],
 				scatter_kws={'s':7},
@@ -101,7 +104,7 @@ ax.set_ylabel(r' Simulation in °C ',fontsize=14)
 ax.legend(loc = 2, bbox_to_anchor = (1,1))
 leg = ax.get_legend()
 L_labels = leg.get_texts()
-for i in range(12):
+for i in range(nmonths):
 	L_labels[(2*i)+1].set_text(r'$R^2:{0:.2f}$'.format(r_value[i]))
 
 # --------------------------------- Save fig -------------------------------
@@ -120,51 +123,54 @@ color_depths = ['#ffffd4','#fee391','#fec44f','#fe9929','#ec7014','#cc4c02',
 label_depths = ['0-40cm','40-80cm','80-120cm','120-160cm','160-200cm','200-320cm',
             	'+320cm']
 
-# ----------------------------- Initialize plot ---------------------------- 
-fig, ax = plt.subplots()
-fig.set_figheight(10)
-fig.set_figwidth(15)
-r_value = np.zeros(np.size(depth_bins)-1)
+# ----------------------------- Initialize plot ----------------------------
+for month in range(nmonths):
+	fig, ax = plt.subplots()
+	fig.set_figheight(10)
+	fig.set_figwidth(15)
+	r_value = np.zeros(np.size(depth_bins)-1)
 
-for i in range(np.size(depth_bins)-1): 								# don't take first one to get 7 bins
-	ind_bin1 = np.where(depth==depth_bins[i])[0][0]
-	ind_bin2 = np.where(depth==depth_bins[i+1])[0][0]
-	
-	slope, intercept, r_value[i], p_value, std_err = stats.linregress(measurement[ind_bin1:ind_bin2],simulation[ind_bin1:ind_bin2])
-	
-	sns.regplot(x = measurement[ind_bin1:ind_bin2],
-				y = simulation[ind_bin1:ind_bin2],
-				color = color_depths[i],
-				label = label_depths[i],
-				scatter_kws={'s':7},
-				line_kws={'label':"Linear Reg"})
-# --------------------------------------------------------------------------
+	for i in range(np.size(depth_bins)-1): 								# don't take first one to get 7 bins
+		ind_bin1 = np.where(depth==depth_bins[i])[0][0]
+		ind_bin2 = np.where(depth==depth_bins[i+1])[0][0]
+		
+		slope, intercept, r_value[i], p_value, std_err = \
+			stats.linregress(measurement[ind_bin1:ind_bin2:nmonths+month],simulation[ind_bin1:ind_bin2:nmonths+month])
+		
+		sns.regplot(x = measurement[ind_bin1:ind_bin2:nmonths+month],
+					y = simulation[ind_bin1:ind_bin2:nmonths+month],
+					color = color_depths[i],
+					label = label_depths[i],
+					scatter_kws={'s':7},
+					line_kws={'label':"Linear Reg"})
+	# --------------------------------------------------------------------------
 
-# ---------------------------- Plot post-options ---------------------------
-# Grid on
-ax.set_xlim(-40,30)
-ax.set_ylim(-40,30)
-plt.plot([-40, 30], [-40, 30], color="gray", linestyle="--", linewidth=1)
-# --------------------------------------------------------------------------
+	# ---------------------------- Plot post-options ---------------------------
+	# Grid on
+	ax.set_xlim(-40,30)
+	ax.set_ylim(-40,30)
+	plt.plot([-40, 30], [-40, 30], color="gray", linestyle="--", linewidth=1)
+	# --------------------------------------------------------------------------
 
-# --------------------------------- Labeling -------------------------------
-ax.set_xlabel(r' Observation in °C',fontsize=14)
-ax.set_ylabel(r' Simulation in °C ',fontsize=14)
-# --------------------------------------------------------------------------
+	# --------------------------------- Labeling -------------------------------
+	ax.set_title(str(label_months[month]),fontsize=14)
+	ax.set_xlabel(r' Observation in °C',fontsize=14)
+	ax.set_ylabel(r' Simulation in °C ',fontsize=14)
+	# --------------------------------------------------------------------------
 
-# --------------------------------- Legend ---------------------------------
-ax.legend(loc = 2, bbox_to_anchor = (1,1))
-leg = ax.get_legend()
-L_labels = leg.get_texts()
-for i in range(np.size(depth_bins)-1):
-	L_labels[(2*i)+1].set_text(r'$R^2:{0:.2f}$'.format(r_value[i]))
+	# --------------------------------- Legend ---------------------------------
+	ax.legend(loc = 2, bbox_to_anchor = (1,1))
+	leg = ax.get_legend()
+	L_labels = leg.get_texts()
+	for i in range(np.size(depth_bins)-1):
+		L_labels[(2*i)+1].set_text(r'$R^2:{0:.2f}$'.format(r_value[i]))
 
-# --------------------------------- Save fig -------------------------------
-plot_name = output_dir + "scatter_depths"
-plt.savefig(plot_name+'.pdf', format='pdf', bbox_inches='tight')
-plt.close()
+	# --------------------------------- Save fig -------------------------------
+	plot_name = output_dir + "scatter_depths.month" + str(month+1)
+	plt.savefig(plot_name+'.pdf', format='pdf', bbox_inches='tight')
+	plt.close()
 
-print("depths scatter plot: done!")
+	print("depths scatter plot month" + str(month+1) + ": done!")
 
 
 

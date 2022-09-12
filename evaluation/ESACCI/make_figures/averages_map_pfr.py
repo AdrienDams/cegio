6b6f7ed89a666a399a1eea3ab785de9e3b8714d9
AdrienDams <adriendamseaux@hotmail.fr>
2022-09-12 12:23:49 +0200

    add orange color

--- evaluation/ESACCI/make_figures/averages_map_pfr.py
+++ evaluation/ESACCI/make_figures/averages_map_pfr.py
@@ -54,13 +54,14 @@ darkgreen  = colors.ListedColormap(['#1a9641'])
 lightgreen = colors.ListedColormap(['#a6d96a'])
 darkred    = colors.ListedColormap(['#d7191c'])
 yellow     = colors.ListedColormap(['#ffffbf'])
+orange     = colors.ListedColormap(['#fdae61'])
 fig = plt.figure(figsize=[5, 5], constrained_layout=True)
 
 ax = fig.add_subplot(1, 1, 1, projection=ccrs.NorthPolarStereo())
 
 # shade variables
 filled1 = ax.pcolormesh(lon, lat, contcont, cmap=darkgreen, transform=ccrs.PlateCarree())
-filled2 = ax.pcolormesh(lon, lat, contfree, cmap=darkred, transform=ccrs.PlateCarree())
+filled2 = ax.pcolormesh(lon, lat, contfree, cmap=orange, transform=ccrs.PlateCarree())
 filled3 = ax.pcolormesh(lon, lat, freecont, cmap=darkred, transform=ccrs.PlateCarree())
 filled4 = ax.pcolormesh(lon, lat, disccont, cmap=lightgreen, transform=ccrs.PlateCarree())
 filled5 = ax.pcolormesh(lon, lat, discfree, cmap=yellow, transform=ccrs.PlateCarree())

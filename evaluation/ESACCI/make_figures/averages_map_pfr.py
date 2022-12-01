fb4f736d4c97d4adc75f36a7ded501a89957a8ec
AdrienDams <adriendamseaux@hotmail.fr>
2022-12-01 15:23:04 +0100

    add permafrost extent calculation (WIP)

--- evaluation/ESACCI/make_figures/averages_map_pfr.py
+++ evaluation/ESACCI/make_figures/averages_map_pfr.py
@@ -13,14 +13,14 @@ import sys
 ctsmfile = sys.argv[1]
 esafile  = sys.argv[2]
 output_name = sys.argv[3]
-#ctsmfile = "/work/aa0049/a271098/cegio/data/ESACCI/57_DOM02_034/CTSM_regridded/PFR.57_DOM02_034.2001.nc"
-#esafile  = "/work/aa0049/a271098/cegio/data/ESACCI/57_DOM02_034/ESACCI_regridded/PFR.57_DOM02_034.2001.nc"
+#ctsmfile = "/work/aa0049/a271098/cegio/data/ESACCI/57_DOM02_040/CTSM_regridded/PFR.57_DOM02_040.2001.nc"
+#esafile  = "/work/aa0049/a271098/cegio/data/ESACCI/57_DOM02_040/ESACCI_regridded/PFR.57_DOM02_040.2001.nc"
 #output_name = "2001"
 variable = "PFR"
 
-
 dctsm = nc.Dataset(ctsmfile, 'r') # read only
 desa  = nc.Dataset(esafile, 'r') # read only
+darea = nc.Dataset("/work/aa0049/a271098/cegio/data/57_regridded.area.PFR.nc", 'r')
 
 # output
 output_dir = os.environ['cegio'] + "/figures/" + os.environ['run_name'] + "/ESACCI/" + variable + "/"
@@ -32,6 +32,8 @@ esa_var  = desa.variables[variable][0,:,:]
 
 lon = dctsm.variables['lon']
 lat = dctsm.variables['lat']
+
+area = np.array(darea.variables['cell_area'])/(6371 * 6371) # convert steradian to km2
 
 # masking
 thre_cont  = 90
@@ -49,6 +51,16 @@ freecont = np.where((esa_free == 1) & (ctsm_continuous == 1),1,np.nan)
 disccont = np.where((esa_dcontinuous == 1) & (ctsm_continuous == 1),1,np.nan)
 discfree = np.where((esa_dcontinuous == 1) & (ctsm_free == 1),1,np.nan)
 
+# calculate permafrost extent area
+ctsm_pfr_area = 0
+esa_pfr_area  = 0
+for i in range(np.shape(ctsm_continuous)[0]):
+	for j in range(np.shape(ctsm_continuous)[1]):
+		if ctsm_continuous[i,j] == 1: 
+			ctsm_pfr_area = area[i,j] + ctsm_pfr_area
+		if (esa_continuous[i,j] == 1 or esa_dcontinuous[i,j] == 1) : 
+			esa_pfr_area = area[i,j] + esa_pfr_area
+
 ## Mapping averages
 darkgreen  = colors.ListedColormap(['#1a9641'])
 lightgreen = colors.ListedColormap(['#a6d96a'])
@@ -61,10 +73,10 @@ ax = fig.add_subplot(1, 1, 1, projection=ccrs.NorthPolarStereo())
 
 # shade variables
 filled1 = ax.pcolormesh(lon, lat, contcont, cmap=darkgreen, transform=ccrs.PlateCarree())
-filled2 = ax.pcolormesh(lon, lat, contfree, cmap=orange, transform=ccrs.PlateCarree())
-filled3 = ax.pcolormesh(lon, lat, freecont, cmap=darkred, transform=ccrs.PlateCarree())
+filled2 = ax.pcolormesh(lon, lat, contfree, cmap=darkred, transform=ccrs.PlateCarree())
+filled3 = ax.pcolormesh(lon, lat, freecont, cmap=yellow, transform=ccrs.PlateCarree())
 filled4 = ax.pcolormesh(lon, lat, disccont, cmap=lightgreen, transform=ccrs.PlateCarree())
-filled5 = ax.pcolormesh(lon, lat, discfree, cmap=yellow, transform=ccrs.PlateCarree())
+filled5 = ax.pcolormesh(lon, lat, discfree, cmap=orange, transform=ccrs.PlateCarree())
 
 # extent map
 ax.set_extent([-180, 180, 90, 57], ccrs.PlateCarree())
@@ -83,6 +95,11 @@ ax.set_boundary(circle, transform=ax.transAxes)
 
 # gridlines labels
 gl = ax.gridlines(draw_labels=True)
+
+# title with pfr extent
+ctsm_area = np.round(ctsm_pfr_area/1e6,3) # round
+esa_area  = np.round(esa_pfr_area/1e6,3)
+ax.set_title('Permafrost extent area: CTSM = %s'%ctsm_area +  ' vs ESACCI = %s'%esa_area +  ' 10e6 m2')
 
 plot_name = output_dir + output_name + ".diff"
 plt.savefig(plot_name +'.pdf', format='pdf', bbox_inches='tight')

f023d7b849d5954d6eba8a9a51e037601d355d7a
AdrienDams <adriendamseaux@hotmail.fr>
2023-01-19 14:22:46 +0100

    reduce redudants code lines, gather 3 plots in a loop

--- evaluation/stations/make_figures/scatter_plot.py
+++ evaluation/stations/make_figures/scatter_plot.py
@@ -78,200 +78,101 @@ plt.close()
 
 print("months scatter plot: done!")
 
-#  PLOT 2: Depths
+#  PLOT 2, 3, 4: Depths, Regions, Decades
 
 # Plot pre-options
 depth_bins   = [0,40,80,120,185,320,np.max(depth)]
-color_depths = ['#fdd0a2','#fdae6b','#fd8d3c','#f16913','#d94801','#8c2d04']
-label_depths = ['0-40cm','40-80cm','80-120cm','120-185cm', '185-320cm',
+depths_color = ['#fdd0a2','#fdae6b','#fd8d3c','#f16913','#d94801','#8c2d04']
+depths_label = ['0-40cm','40-80cm','80-120cm','120-185cm', '185-320cm',
             	'+320cm']
 
-# Initialize plot
-for month in range(nmonths):
-	fig, ax = plt.subplots()
-	fig.set_figheight(10)
-	fig.set_figwidth(15)
-	r_value = np.zeros(np.size(depth_bins)-1)
-
-	for i in range(np.size(depth_bins)-1): # don't take first one to get 6 bins
-		ind_bin1 = np.where(depth==depth_bins[i])[0][0]
-		ind_bin2 = np.where(depth==depth_bins[i+1])[0][0]
-		
-		slope, intercept, r_value[i], p_value, std_err = \
-			stats.linregress(measurement[ind_bin1:ind_bin2:nmonths+month],simulation[ind_bin1:ind_bin2:nmonths+month])
-		
-		sns.regplot(x = measurement[ind_bin1:ind_bin2:nmonths+month],
-					y = simulation[ind_bin1:ind_bin2:nmonths+month],
-					color = color_depths[i],
-					label = label_depths[i],
-					scatter_kws={'s':5,'alpha':0.6},
-					line_kws={'label':"Linear Reg"})
-
-	# Plot post-options
-
-	# Grid on
-	ax.set_xlim(-40,30)
-	ax.set_ylim(-40,30)
-	plt.plot([-40, 30], [-40, 30], color="gray", linestyle="--", linewidth=1)
-
-	# Labeling
-	ax.set_title(str(label_months[month]),fontsize=14)
-	ax.set_xlabel(r' Observation in °C',fontsize=14)
-	ax.set_ylabel(r' Simulation in °C ',fontsize=14)
-
-	# Legend
-	ax.legend(loc = 2, bbox_to_anchor = (1,1))
-	leg = ax.get_legend()
-	L_labels = leg.get_texts()
-	for i in range(np.size(depth_bins)-1):
-		L_labels[(2*i)+1].set_text(r'$R^2:{0:.2f}$'.format(r_value[i]))
-
-	# Save fig
-	plot_name = output_dir + "scatter_depths.month" + str(month+1)
-	plt.savefig(plot_name+'.pdf', format='pdf', bbox_inches='tight')
-	plt.close()
-
-	print("depths scatter plot month" + str(month+1) + ": done!")
-
-#  PLOT 3: Regions
-
-# Reorder data, get indices of elements in station_lon sorted in ascending order
-sort_indices = np.argsort(station_lon)
-
-# Sort the arrays using the sort indices
-year_lonsrt 		= np.array(year)[sort_indices]
-depth_lonsrt 		= np.array(depth)[sort_indices]
-station_lon_lonsrt 	= np.array(station_lon)[sort_indices]
-station_lat_lonsrt 	= np.array(station_lat)[sort_indices]
-measurement_lonsrt 	= np.array(measurement)[sort_indices]
-simulation_lonsrt	= np.array(simulation)[sort_indices]
-
-
-# Plot pre-options
 spatial_bins  = [-180,-140,0,50,80,125,180]
-color_spatial = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33']
-label_spatial = ['Alaska','Canadian Archipelago','European Russia','Western Siberia', \
+spatial_color = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33']
+spatial_label = ['Alaska','Canadian Archipelago','European Russia','Western Siberia', \
 					'Central Siberia', 'Eastern Siberia']
 
-# Initialize plot
-for month in range(nmonths):
-	fig, ax = plt.subplots()
-	fig.set_figheight(10)
-	fig.set_figwidth(15)
-	r_value = np.zeros(np.size(spatial_bins)-1)
-
-	for i in range(np.size(spatial_bins)-1): # don't take first one to get 6 bins
-		# Calculate the absolute difference between the target longitude and each element in the station_lon array
-		diff1 = np.abs(station_lon_lonsrt-spatial_bins[i])
-		diff2 = np.abs(station_lon_lonsrt-spatial_bins[i+1])
-		# Find the index of the minimum difference (i.e. the closest longitude)
-		ind_bin1 = np.argmin(diff1)
-		ind_bin2 = np.argmin(diff2)
-		
-		slope, intercept, r_value[i], p_value, std_err = \
-			stats.linregress(measurement[ind_bin1:ind_bin2:nmonths+month],simulation[ind_bin1:ind_bin2:nmonths+month])
-		
-		sns.regplot(x = measurement[ind_bin1:ind_bin2:nmonths+month],
-					y = simulation[ind_bin1:ind_bin2:nmonths+month],
-					color = color_spatial[i],
-					label = label_spatial[i],
-					scatter_kws={'s':5,'alpha':0.6},
-					line_kws={'label':"Linear Reg"})
-
-	# Plot post-options
-
-	# Grid on
-	ax.set_xlim(-40,30)
-	ax.set_ylim(-40,30)
-	plt.plot([-40, 30], [-40, 30], color="gray", linestyle="--", linewidth=1)
-
-	# Labeling
-	ax.set_title(str(label_months[month]),fontsize=14)
-	ax.set_xlabel(r' Observation in °C',fontsize=14)
-	ax.set_ylabel(r' Simulation in °C ',fontsize=14)
-
-	# Legend
-	ax.legend(loc = 2, bbox_to_anchor = (1,1))
-	leg = ax.get_legend()
-	L_labels = leg.get_texts()
-	for i in range(np.size(spatial_bins)-1):
-		L_labels[(2*i)+1].set_text(r'$R^2:{0:.2f}$'.format(r_value[i]))
-
-	# Save fig
-	plot_name = output_dir + "scatter_spatial.month" + str(month+1)
-	plt.savefig(plot_name+'.pdf', format='pdf', bbox_inches='tight')
-	plt.close()
-
-	print("spatial scatter plot month" + str(month+1) + ": done!")
-
-#  PLOT 4: Decades
-
-# Reorder data, get indices of elements in year sorted in ascending order
-sort_indices = np.argsort(year)
-
-# Sort the arrays using the sort indices
-year_timsrt 		= np.array(year)[sort_indices]
-depth_timsrt 		= np.array(depth)[sort_indices]
-station_lon_timsrt 	= np.array(station_lon)[sort_indices]
-station_lat_timsrt 	= np.array(station_lat)[sort_indices]
-measurement_timsrt 	= np.array(measurement)[sort_indices]
-simulation_timsrt	= np.array(simulation)[sort_indices]
-
-# Plot pre-options
-decades = range(np.min(year_timsrt), np.max(year_timsrt),10)
-if decades[-1] != np.max(year_timsrt):
-	temporal_bins = np.append(decades,np.max(year_timsrt))
+decades = range(np.min(year), np.max(year),10)
+if decades[-1] != np.max(year):
+	temporal_bins = np.append(decades,np.max(year))
 else:
 	temporal_bins = decades 
 
-all_color_temporal = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33']
-color_temporal = all_color_temporal[0:np.size(temporal_bins)-1]
-label_temporal = ['1980-1990','1990-2000','2000-2010','2010-2020']
+temporal_color_all = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33']
+temporal_color = temporal_color_all[0:np.size(temporal_bins)-1]
+temporal_label = ['1980-1990','1990-2000','2000-2010','2010-2020']
+
+# create lists comprehension for looping
+bins = [depth_bins, spatial_bins, temporal_bins]
+colors = [depths_color, spatial_color, temporal_color]
+labels = [depths_label, spatial_label, temporal_label]
+key_variables = [depth, station_lon, year]
+key_variables_name = ["depths", "spatial", "temporal"]
 
 # Initialize plot
-for month in range(nmonths):
-	fig, ax = plt.subplots()
-	fig.set_figheight(10)
-	fig.set_figwidth(15)
-	r_value = np.zeros(np.size(temporal_bins)-1)
+for variable_type in [0, 1, 2]:
+	# Reorder data, get indices of elements in station_lon sorted in ascending order
+	sort_indices = np.argsort(key_variables[variable_type])
 
-	for i in range(np.size(temporal_bins)-1): # don't take first one to get 6 bins
-		ind_bin1 = np.where(year_timsrt==temporal_bins[i])[0][0]
-		ind_bin2 = np.where(year_timsrt==temporal_bins[i+1])[0][0]
-		
-		slope, intercept, r_value[i], p_value, std_err = \
-			stats.linregress(measurement[ind_bin1:ind_bin2:nmonths+month],simulation[ind_bin1:ind_bin2:nmonths+month])
-		
-		sns.regplot(x = measurement[ind_bin1:ind_bin2:nmonths+month],
-					y = simulation[ind_bin1:ind_bin2:nmonths+month],
-					color = color_temporal[i],
-					label = label_temporal[i],
-					scatter_kws={'s':5,'alpha':0.6},
-					line_kws={'label':"Linear Reg"})
+	# Sort the arrays using the sort indices
+	year_srt 		= np.array(year)[sort_indices]
+	depth_srt 		= np.array(depth)[sort_indices]
+	station_lon_srt = np.array(station_lon)[sort_indices]
+	station_lat_srt = np.array(station_lat)[sort_indices]
+	measurement_srt = np.array(measurement)[sort_indices]
+	simulation_srt	= np.array(simulation)[sort_indices]
 
-	# Plot post-options
+	key_variables_srt = [depth_srt, station_lon_srt, year_srt] # update list (TO-DO: need better approach)
 
-	# Grid on
-	ax.set_xlim(-40,30)
-	ax.set_ylim(-40,30)
-	plt.plot([-40, 30], [-40, 30], color="gray", linestyle="--", linewidth=1)
+	for month in range(nmonths):
+		fig, ax = plt.subplots()
+		fig.set_figheight(10)
+		fig.set_figwidth(15)
+		r_value = np.zeros(np.size(bins[variable_type])-1)
 
-	# Labeling
-	ax.set_title(str(label_months[month]),fontsize=14)
-	ax.set_xlabel(r' Observation in °C',fontsize=14)
-	ax.set_ylabel(r' Simulation in °C ',fontsize=14)
+		for i in range(np.size(bins[variable_type])-1): # don't take first one to get 6 bins
+			if variable_type == 1:
+				# Calculate the absolute difference between the target longitude and each element in the station_lon array
+				diff1 = np.abs(key_variables_srt[variable_type]-bins[variable_type][i])
+				diff2 = np.abs(key_variables_srt[variable_type]-bins[variable_type][i+1])
+				# Find the index of the minimum difference (i.e. the closest longitude)
+				ind_bin1 = np.argmin(diff1)
+				ind_bin2 = np.argmin(diff2)
+			else:
+				ind_bin1 = np.where(key_variables_srt[variable_type]==bins[variable_type][i])[0][0]
+				ind_bin2 = np.where(key_variables_srt[variable_type]==bins[variable_type][i+1])[0][0]
+			
+			slope, intercept, r_value[i], p_value, std_err = \
+				stats.linregress(measurement_srt[ind_bin1:ind_bin2:nmonths+month],simulation_srt[ind_bin1:ind_bin2:nmonths+month])
+			
+			sns.regplot(x = measurement_srt[ind_bin1:ind_bin2:nmonths+month],
+						y = simulation_srt[ind_bin1:ind_bin2:nmonths+month],
+						color = colors[variable_type][i],
+						label = labels[variable_type][i],
+						scatter_kws={'s':5,'alpha':0.6},
+						line_kws={'label':"Linear Reg"})
 
-	# Legend
-	ax.legend(loc = 2, bbox_to_anchor = (1,1))
-	leg = ax.get_legend()
-	L_labels = leg.get_texts()
-	for i in range(np.size(temporal_bins)-1):
-		L_labels[(2*i)+1].set_text(r'$R^2:{0:.2f}$'.format(r_value[i]))
+		# Plot post-options
 
-	# Save fig
-	plot_name = output_dir + "scatter_temporal.month" + str(month+1)
-	plt.savefig(plot_name+'.pdf', format='pdf', bbox_inches='tight')
-	plt.close()
+		# Grid on
+		ax.set_xlim(-40,30)
+		ax.set_ylim(-40,30)
+		plt.plot([-40, 30], [-40, 30], color="gray", linestyle="--", linewidth=1)
 
-	print("temporal scatter plot month" + str(month+1) + ": done!")
+		# Labeling
+		ax.set_title(str(label_months[month]),fontsize=14)
+		ax.set_xlabel(r' Observation in °C',fontsize=14)
+		ax.set_ylabel(r' Simulation in °C ',fontsize=14)
 
+		# Legend
+		ax.legend(loc = 2, bbox_to_anchor = (1,1))
+		leg = ax.get_legend()
+		L_labels = leg.get_texts()
+		for i in range(np.size(bins[variable_type])-1):
+			L_labels[(2*i)+1].set_text(r'$R^2:{0:.2f}$'.format(r_value[i]))
+
+		# Save fig
+		plot_name = output_dir + "scatter_" + str(key_variables_name[variable_type]) + ".month" + str(month+1)
+		plt.savefig(plot_name+'.pdf', format='pdf', bbox_inches='tight')
+		plt.close()
+
+		print(str(key_variables_name[variable_type]) + " scatter plot month" + str(month+1) + ": done!")

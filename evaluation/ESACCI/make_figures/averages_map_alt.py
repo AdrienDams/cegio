select exclusively period average for ESACCI mapping, small fixes and improvements

evaluation/ESACCI/make_figures/averages_map_alt.py
@@ -15,9 +15,6 @@ import sys
 ctsmfile = sys.argv[1]
 esafile  = sys.argv[2]
 output_name = sys.argv[3] + "_ESAvsCTSM_ALT"
-#ctsmfile = "/work/aa0049/a271098/cegio/data/ESACCI/57_DOM02_004/CTSM_regridded/ALT.57_DOM02_004.1997.nc"
-#esafile  = "/work/aa0049/a271098/cegio/data/ESACCI/57_DOM02_004/ESACCI_regridded/ALT.ESACCI.on.57_DOM02_004.1997.nc"
-#output_name = "1997"
 variable = "ALT"
 
 dctsm = nc.Dataset(ctsmfile, 'r') # read only
@@ -28,8 +25,8 @@ output_dir = os.environ['cegio'] + "/figures/" + os.environ['run_name'] + "/ESAC
 os.makedirs(output_dir, exist_ok=True)
 
 # extract variables
-ctsm_var = dctsm.variables[variable][:,:] 
-esa_var  = desa.variables[variable][0,:,:] # remove useless dimension
+ctsm_var = dctsm.variables[variable][0,:,:] # remove useless dimension
+esa_var  = desa.variables[variable][0,:,:] 
 
 # only take esa where ctsm exists
 esa_var_test = np.ma.masked_array(esa_var, mask=ctsm_var.mask == False)
@@ -44,7 +41,7 @@ all_var = [ctsm_var, esa_var]
 
 ## Mapping averages
 cmap_top = 6
-fig = plt.figure(figsize=[10, 5], constrained_layout=True)
+fig = plt.figure(figsize=[16, 8], constrained_layout=True)
 nax = 0
 
 for ax in ["ax1", "ax2"]:
@@ -92,7 +89,7 @@ plt.close()
 
 ## Mapping diff
 cmap_top = 6
-fig = plt.figure(figsize=[5, 5], constrained_layout=True)
+fig = plt.figure(figsize=[8, 8], constrained_layout=True)
 
 ax = fig.add_subplot(1, 1, 1, projection=ccrs.NorthPolarStereo())
 
@@ -104,7 +101,7 @@ filled = ax.pcolormesh(lon, lat, diff_var, cmap='RdBu_r',
 # extent map
 ax.set_extent([-180, 180, 90, 57], ccrs.PlateCarree())
 
-ax.set_title("difference")
+ax.set_title("CTSM - ESA-CCI")
 
 # draw land and ocean
 ax.add_feature(cartopy.feature.OCEAN)

evaluation/ESACCI/make_figures/averages_map_pfr.py
@@ -62,7 +62,7 @@ desa_pextent  = nc.Dataset(esa_pextent_file, 'r')
 dctsm_area = nc.Dataset(ctsm_area_file, 'r')
 
 # extract variables
-ctsm_pextent = dctsm_pextent.variables['TSOI'][0,:]-273.15 # remove useless dimension
+ctsm_pextent = dctsm_pextent.variables['TSOI'][0,0,:]-273.15 # remove useless dimension
 esa_pextent  = desa_pextent.variables[variable][0,:,:]
 ctsm_area = np.array(dctsm_area.variables['cell_area'])/1e6 # convert m2 to km2
 
@@ -80,7 +80,7 @@ lightgreen = colors.ListedColormap(['#a6d96a'])
 darkred    = colors.ListedColormap(['#d7191c'])
 yellow     = colors.ListedColormap(['#ffffbf'])
 blue       = colors.ListedColormap(['#0571b0'])
-fig = plt.figure(figsize=[5, 5], constrained_layout=True)
+fig = plt.figure(figsize=[8, 8], constrained_layout=True)
 
 ax = fig.add_subplot(1, 1, 1, projection=ccrs.NorthPolarStereo())
 
@@ -112,7 +112,7 @@ gl = ax.gridlines(draw_labels=True)
 # title with pfr extent
 ctsm_area = np.round(ctsm_pfr_area/1e6,3) # round
 esa_area  = np.round(esa_pfr_area/1e6,3)
-ax.set_title('Permafrost extent area: CTSM = %s'%ctsm_area +  ' - ESACCI = %s'%esa_area +  ' 10e6 km2')
+ax.set_title('Permafrost extent area:\n CTSM = %s'%ctsm_area +  ' - ESACCI = %s'%esa_area +  ' $10e6 km^{2}$')
 
 plot_name = output_dir + output_name + ".diff"
 plt.savefig(plot_name +'.pdf', format='pdf', bbox_inches='tight')